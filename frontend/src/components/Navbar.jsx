0476ebe22f7c7d6efcf98cfb4d231260c15fe3c7
PratyakshaAmbig <pratyaksha1015@gmail.com>
2025-09-09 14:32:45 +0530

    Collection section design and functionality is implemented in the frontend section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
+  const {showSearch, setShowSearch} = useContext(ShopContext)
   const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -26,7 +28,7 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} alt='' className='w-5 cursor-pointer'/>
+        <img onClick={()=>setShowSearch(!showSearch)} src={assets.search_icon} alt='' className='w-5 cursor-pointer'/>
 
         <div className='group relative'>
           <img src={assets.profile_icon} alt='' className='w-5 cursor-pointer'/>
